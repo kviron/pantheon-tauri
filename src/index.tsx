7c2c55d1a3ebe274c3b326f17adf4f79c34f46f5
Мамаев Роман <mamaev@myservices.digital>
2024-06-07 11:41:30 +0300

    Добавил первоначальную цветовую гамму

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 /* @refresh reload */
 import 'reset-css';
+import '@/app/styles/index.scss';
 import { render } from 'solid-js/web';
 import App from './app/App.tsx';
 import { ThemeProvider } from '@/app/providers/ThemeProvider/ui/ThemeProvider.tsx';
